5170a50b4cd95c611a0fc566283c8406c4b7efc1
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-24 16:26:47 +0530

    more refactoring

---
+++ App/controllers/ErrorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class ErrorController
+{
+    public function notFound($message = 'Resource not found')
+    {
+        http_response_code(404);
+        loadView('error', [
+            'status' => 404,
+            'message' => $message
+        ]);
+    }
+    public function unauthorized($message = 'You are not authorized to access this resource')
+    {
+        http_response_code(403);
+        loadView('error', [
+            'status' => 403,
+            'message' => $message
+        ]);
+    }
+}
