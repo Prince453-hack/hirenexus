c3457cbf31a6ca141dd9162bb9282ce05f996928
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-25 22:20:42 +0530

    edit form

--- App/views/listings/show.views.php
+++ App/views/listings/show.views.php
@@ -19,7 +19,7 @@
                 Back To Listings
             </a>
             <div class="flex space-x-4 ml-4">
-                <a href="/edit" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+                <a href="/listings/edit/<?= $listing->id ?>" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
                 <!-- Delete Form -->
                 <form method="POST" onsubmit="return confirmDelete()">
                     <input type="hidden" name="_method" value="DELETE">
