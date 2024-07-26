289fc700f64e54315b298cf9a1881e557db2cefa
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-26 16:23:54 +0530

    users registration validation

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -24,4 +24,58 @@ class UserController
     {
         loadView('users/create');
     }
+
+    public function store()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $passwordConfirmation = $_POST['password_confirmation'];
+
+        $errors = [];
+
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Invalid email address';
+        }
+
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = 'Name must be between 2 and 50 characters';
+        }
+
+        if (!Validation::string($password, 6, 50)) {
+            $errors['password'] = 'Password must be at least 6 characters';
+        }
+
+        if (!Validation::match($password, $passwordConfirmation)) {
+            $errors['password_confirmation'] = 'Password do not match';
+        }
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state
+                ]
+            ]);
+            exit;
+        }
+
+        $params = [
+            'email' => $email,
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params);
+
+        if ($user) {
+            $errors['email'] = 'Email already exists';
+            loadView('users/create', [
+                'errors' => $errors,
+            ]);
+        }
+    }
 }
